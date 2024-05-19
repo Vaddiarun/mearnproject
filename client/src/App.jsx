59d4c816a8fdaae682fb41ee70285b48e1791881
arunsvaddi <arunsvaddi@gmail.com>
2024-05-19 15:51:19 +0530

    r

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { BrowserRouter,Route,Routes} from "react-router-dom"
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile  from './pages/Profile';
+import Signup from './pages/Signup';
+import Sigin from './pages/Sigin';
 
 export default function App() {
   return (
-    <div>vaddi arun frist project</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/About" element={<About />}/>
+
+      <Route path="/Profile" element={<Profile/>}/>
+      <Route path="/Sigin" element={<Sigin/>}/>
+      <Route path="/Signup" element={<Signup/>}/>
+
+    </Routes>
+
+  
+
+
+    </BrowserRouter>
   )
 }
