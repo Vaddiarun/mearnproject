def467461ac3eabd3f4cea266da0c536128bced2
arunsvaddi <arunsvaddi@gmail.com>
2024-05-20 10:27:11 +0530

    schema addded

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-import { configDotenv } from 'dotenv';
+import dotenv from 'dotenv';
 import express from 'express';
 
 import mongoose from 'mongoose';
 dotenv.config();
 
-mongoose.connect(process.env.MANGO).then(()=>{
+mongoose.connect("mongodb+srv://arunsvaddi:Arun402001@mernproject.plhey56.mongodb.net/?retryWrites=true&w=majority&appName=mernproject").then(()=>{
     console.log("connected to Mongodb")
 }).catch((error)=>{
     console.log(error)
 })
 
-mongodb+srv://arunsvaddi:<password>@mernproject.plhey56.mongodb.net/?retryWrites=true&w=majority&appName=mernproject
+//mongodb+srv://arunsvaddi:<password>@mernproject.plhey56.mongodb.net/?retryWrites=true&w=majority&appName=mernproject
 const app=express();
 
 app.listen(3000,()=>{
